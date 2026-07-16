8d369d8202d5d1597f53087e2a49cc9aab11c334
tomerkrivzki19 <tomerkrivizki@gmail.com>
2026-07-16 17:39:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner"); //creating a temporary links => we are making this becouse of security reason and also by doing that we are avoiding enjected files or more to the link
+const { randomUUID } = require("crypto");
+
+const app = express();
+const PORT = 3000;
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+const BUCKET = process.env.S3_BUCKET;
+
+// Multer keeps the uploaded file temporarily in RAM.
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
+
+const posts = [];
+
+app.use(express.static("public")); //making a configuration to the server that it will accept a static filles > static -> things that will not change when the server is running | also it is a shorcut for the path.... ( avoiding importing all the times )
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+  }),
+);
+// putting multer as a middlware
+app.use("/upload", upload.single("file"));
+
+app.post("/upload", async (req, res) => {
+  try {
+    console.log("Content-Type:", req.headers["content-type"]);
+    console.log("Body:", req.body);
+    console.log("File:", req.file);
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: "No file was received",
+      });
+    }
+
+    const unique = randomUUID() + "-" + req.file.originalname; //will make new name for a new file before its saving in the bucket
+
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: BUCKET,
+        Key: unique,
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+      }),
+
+      posts.unshift({ unique, description: req.body.description || "" }), //unshift=> a methoda that will add to the array each time ....
+    );
+
+    res.status(201).json({ message: "uploades to S3 successfully" });
+  } catch (error) {
+    console.error("Upload error:", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
---
+++ public/script.js
@@ -0,0 +1,42 @@
+const uploadForm = document.getElementById("uploadForm");
+
+uploadForm.addEventListener("submit", uploadFile);
+
+async function uploadFile(event) {
+  // Prevent the form from refreshing the page
+  event.preventDefault();
+
+  try {
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
+
+    if (!file) {
+      alert("Please select a file first.");
+      return;
+    }
+
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    console.log("formData", formData);
+for (const value of formData.values()) {
+  console.log(value);
+}
+    const response = await fetch("http://127.0.0.1:3000/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    console.log("Success:", data);
+  } catch (error) {
+    console.error("Upload failed:", error);
+  }
+}
